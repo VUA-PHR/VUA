492bc73d5cb9e6d6679da5b72a5a7b2c1afcb3ce
VUA-PHR <vua@ph-r.com>
2026-09-05 06:11:54 +0800

    chore: remove temporary debug scripts

--- .tmp-crash-query.ps1
+++
@@ -1,6 +0,0 @@
-Get-WinEvent -FilterHashtable @{LogName='Application'; Id=1000} -MaxEvents 3 -ErrorAction SilentlyContinue | ForEach-Object {
-  Write-Output ("TIME " + $_.TimeCreated.ToString("HH:mm:ss"))
-  $lines = $_.Message -split "`n"
-  Write-Output ($lines[0..8] -join " | ")
-  Write-Output "----"
-}
